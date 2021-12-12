c9a1580b31e2d39364ca438d93dfa1d1821c59a0
wannvmi <wannvmi@163.com>
2021-12-12 21:35:12 +0800

    update v

--- packages/renderer/src/index.ts
+++ packages/renderer/src/index.ts
@@ -8,6 +8,4 @@ import { createApp } from 'vue';
 import App from '/@/App.vue';
 import router from '/@/router';
 
-createApp(App)
-  .use(router)
-  .mount('#app');
+createApp(App).use(router).mount('#app');
